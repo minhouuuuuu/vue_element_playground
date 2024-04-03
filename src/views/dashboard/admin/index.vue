e7ec97c5c0c58899afa995f7ad8c04cee2a450c8
Minh NGUYEN <minhstrasbourg@gmail.com>
2024-04-03 03:04:41 +0200

    first commit

---
+++ src/views/dashboard/admin/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="dashboard-editor-container">
+    <el-row>
+      <el-button>Défaut</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Stop Daddy</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
+export default {
+  name: 'DashboardAdmin',
+  data() {
+    return {
+      lineChartData: lineChartData.newVisitis
+    }
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .dashboard-editor-container {
+    padding: 64px;
+    background-color: rgb(240, 242, 245);
+    position: relative;
+  }
+  .el-row{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>
